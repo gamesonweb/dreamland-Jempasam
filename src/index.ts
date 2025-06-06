14c5f56c79b01021d9f540f4a274afe135c385b4
Jempasam <samueldemont83@gmail.com>
2025-06-06 11:15:35 +0200

    Load level by name in query variable

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,13 @@ import { html } from "sam-lib"
 
 
 // Ask file
-const fileInput = html.a`<input id="file" type="file" accept=".glb" />` as HTMLInputElement
-
-document.body.replaceChildren(fileInput)
-
-const file = await new Promise<File>(resolve=>{
-    fileInput.oninput = (e) => {
-        resolve(fileInput.files!!.item(0))
-    }
-})
+const levels = import.meta.glob("../levels/*.glb",{eager:true, import:"default", query:"?url"})
+const level_name = new URLSearchParams(window.location.search).get("level") || "level.glb" 
+const level_url = levels[`../levels/${level_name}.glb`] as string
+if(!level_url){
+    document.body.replaceChildren(html`<h1>Level not found</h1>`)
+    throw new Error(`Level not found: ${level_name}`)
+}
 
 // Init everything
 const canvas = html.a`<canvas id="game" style="width:100%;height:100%;background-color: red;"></canvas>` as HTMLCanvasElement
@@ -36,7 +34,7 @@ const manager = new GameManager()
 manager.addBehaviour("solid", new SolidBehaviour())
 manager.addBehaviour("marble", new MarbleBehaviour())    
 
-const result = (await ImportMeshAsync(file,scene))
+const result = (await ImportMeshAsync(level_url,scene))
 const root = result.meshes.find(m=>m.name=="__root__")
 //for(const node of result.meshes) manager.loadObjects(node)
 //for(const node of result.transformNodes) manager.loadObjects(node)
