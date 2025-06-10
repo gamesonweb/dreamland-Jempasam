298108fdcf0c9fbea9531e98b2622148ac94d70b
Jempasam <samueldemont83@gmail.com>
2025-06-10 14:21:02 +0200

    relative link buied '

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 export default ({ mode }) => {
     const root = process.cwd()
     return defineConfig({
+      base: "./",
       optimizeDeps: {
         exclude: ['@babylonjs/havok']
       }
